9b09dbf01d14cc2c54523fb76aeb6f16308d342a
mohammedsadiquddin <mohammedsadiquddin287@gmail.com>
2022-02-26 16:02:56 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+let flag = 0;
+slideShow(flag);
+function slideShow(num){
+    let slides=document.getElementsByClassName('slide');
+    if(num == slides.length){
+        flag = 0;
+        num = 0;
+    }
+    if(num <0){
+        flag = slides.length - 1;
+        num = slides.length - 1;
+    }
+    for(let y of slides){
+        y.style.display = 'none';
+    }
+    slides[num].style.display = 'block'; 
+}
+function controller(x){
+    flag = flag + x;
+    slideShow(flag);
+}
